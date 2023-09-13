6405377ea5c1f079420d2e8ae6389f4134f55b82
Leandro090685 <Leandro090685@github.com>
2023-09-13 14:27:01 -0300

    advanced fields and graph spacemision

--- user/spacemision/models/mision.py
+++ user/spacemision/models/mision.py
@@ -8,3 +8,12 @@ class Mision(models.Model):
     members_crew = fields.Many2one(comodel_name="res.partner", string='Members Crew', ondelete='restrict')
     launch_date = fields.Datetime(string='Launch Date')
     return_date = fields.Datetime(string='Return Date')
+    quantity_fuel = fields.Float(string="Quantity Fuel")
+    miles = fields.Float(string="Miles", compute="_compute_total_miles", store = True)
+    number_engine = fields.Integer(string="Numbers of motors")
+
+    
+    @api.depends("quantity_fuel")
+    def _compute_total_miles(self):
+        for record in self:
+            record.miles = record.quantity_fuel * 3
